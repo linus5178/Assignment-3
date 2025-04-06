7bef4a1d078a69446489e0d728c57895e53fcea8
linus5178 <109713775+linus5178@users.noreply.github.com>
2025-04-06 17:18:02 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,6 @@
+import math
+number=int(input("Enter a number: "))
+print("Your number",number)
+print("Square root ",math.sqrt(number))
+print("Logarithm ",math.log(number))
+print("Sine ",math.sin(number))
